9d178a388d48c415275221f6149f512317971700
DeltaNedas <deltanedas@gmail.com>
2019-10-12 21:48:01 +0100

    more boilerplate

--- src/main/java/deltanedas/thezoneproject/blocks/BaseSkull.java
+++ src/main/java/deltanedas/thezoneproject/blocks/BaseSkull.java
@@ -5,14 +5,14 @@ import net.minecraft.block.BlockSkull;
 import net.minecraft.item.ItemBlock;
 
 public class BaseSkull extends BlockSkull {
-	
+
 	public BaseSkull(String name) {
 		super();
 		name = "skull_" + name;
 		setUnlocalizedName(name);
 		setRegistryName(name);
 		setCreativeTab(TheZoneProject.modTab);
-		
+
 		TheZoneProject.BLOCKS.add(this);
 		TheZoneProject.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
 	}
--- src/main/java/deltanedas/thezoneproject/init/InitEntities.java
+++ src/main/java/deltanedas/thezoneproject/init/InitEntities.java
@@ -3,20 +3,22 @@ package deltanedas.thezoneproject.init;
 import deltanedas.thezoneproject.Constants;
 import deltanedas.thezoneproject.TheZoneProject;
 import deltanedas.thezoneproject.entities.EntityBlindWolf;
+import deltanedas.thezoneproject.entities.EntityBloodsucker;
+// import deltanedas.thezoneproject.entities.EntityPseudowolf;
 import deltanedas.thezoneproject.entities.EntitySnork;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class InitEntities {
-	
+
 	public InitEntities() {
 		EntityBlindWolf.registerEntity();
 		//Pseudowolf.registerEntity();
 		EntitySnork.registerEntity();
-		//Bloodsucker.registerEntity();
+		Bloodsucker.registerEntity();
 	}
-	
+
 	public static void registerEntity(String name, Class<? extends Entity> entity, int id, int range, int colourPrimary, int colourSecondary) {
 		EntityRegistry.registerModEntity(new ResourceLocation(Constants.MOD_ID + ":" + name), entity, name, id, TheZoneProject.instance, range, 1, true, colourPrimary, colourSecondary);
 	}
